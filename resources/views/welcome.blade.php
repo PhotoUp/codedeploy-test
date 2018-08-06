381e5ac39fe5c249f139660f9cd74ae9f602552f
Gerald Alinio <38755420+gerald1992@users.noreply.github.com>
2018-08-06 19:11:53 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Inday Laravel
+                    Inday Laravel Bisaya
                 </div>
 
                 <div class="links">
