ba6377e1d18a64e3d1183a638a3dbc0a9a646711
Gerald Alinio <38755420+gerald1992@users.noreply.github.com>
2018-08-07 15:49:08 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Inday Lara Taga Cebu On The Mix
+                    Inday Lara On The Mix
                 </div>
 
                 <div class="links">
