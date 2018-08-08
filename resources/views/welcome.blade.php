548a5b6cdb286a9ad354a919f14985be174cfe8d
Gerald Alinio <38755420+gerald1992@users.noreply.github.com>
2018-08-08 17:10:45 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Live</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    DJ Larang On The Mix
+                    DJ Eeehm On The Mix
                 </div>
 
                 <div class="links">
